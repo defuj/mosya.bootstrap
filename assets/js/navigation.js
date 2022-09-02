7e9d7ce59ce8d2fca27a1cd8c8fb3504e5bf3e77
defuj <defuj.official@gmail.com>
2022-09-02 14:07:47 +0700

    update search page

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -19,12 +19,12 @@ navItems.forEach(function(e, i) {
 });
 
 // tab naigation
-var menuTabs = document.querySelectorAll(".menu-tab-item");
-menuTabs.forEach(function(e, i) {
-    e.addEventListener("click", function(e) {
-    menuTabs.forEach(function(e2, i2) {
-        e2.classList.remove("active");
-    });
-    this.classList.add("active");
-    });
-});
+// var menuTabs = document.querySelectorAll(".menu-tab-item");
+// menuTabs.forEach(function(e, i) {
+//     e.addEventListener("click", function(e) {
+//     menuTabs.forEach(function(e2, i2) {
+//         e2.classList.remove("active");
+//     });
+//     this.classList.add("active");
+//     });
+// });
