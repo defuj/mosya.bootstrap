b778eab0b5d39ea7a980b9ed33bbf53d23d724bb
defuj <defuj.official@gmail.com>
2022-09-06 11:18:50 +0700

    update support pwa

--- assets/js/session.js
+++ assets/js/session.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js");
+    // navigator.serviceWorker.register("service-worker.js");
 }
 
 const spinnerWhite = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
